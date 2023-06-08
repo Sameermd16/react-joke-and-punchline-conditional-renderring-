1eaf53d818bf6192edfdac0b4233ea32619500b3
sameermd16 <sameerthemc@gmail.com>
2023-06-08 21:01:53 +0530

    conditional renderring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import Joke from './components/Joke';
+import jokesData from './jokesData';
 import './App.css';
 
 function App() {
+
+  const jokeElements = jokesData.map((joke)=> {
+    return (
+      <Joke 
+        key = {joke.id}
+        setup = {joke.setup}
+        punchline = {joke.punchline}
+      />
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {jokeElements}
     </div>
   );
 }
---
+++ src/components/Joke.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false);
+
+    function toggleShown(){
+        setIsShown((prevShown)=> {
+            return !prevShown
+        })
+    }
+
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p> {props.punchline} </p>}
+            <button onClick={toggleShown}>show punchline</button>
+            <hr />
+        </div>
+    )
+}
+
+export default Joke;
